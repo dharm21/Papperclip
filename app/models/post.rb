04c124e3dfe151cc255cbe4ad450d17ad7c201d7
dharm21 <dharmendrayuvasoft189@gmail.com>
2017-04-28 18:27:56 +0530

    Upload images

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  has_attached_file :photo
+  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
+end
